65a1b6fb3789acb280b4c2b023b6f94456429f57
psh999 <pranavhoskere@gmail.com>
2026-03-21 18:58:04 -0700

    Initial commit for asteroids using pygame

---
+++ main.py
@@ -0,0 +1,30 @@
+import pygame
+import constants 
+
+from constants import SCREEN_HEIGHT, SCREEN_WIDTH
+from logger import log_state
+
+def main():
+    print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
+    pygame.init()
+    clock = pygame.time.Clock()
+    dt = 0
+
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    while True: 
+        log_state()
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        screen.fill('black')
+        pygame.display.flip()   
+        dt = clock.tick(60)/1000
+
+    
+
+
+if __name__ == "__main__":
+    main()
+    
