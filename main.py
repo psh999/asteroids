237862ebfd467a75c4f7e0292bcaaa78ff8ae100
psh999 <pranavhoskere@gmail.com>
2026-03-22 12:12:18 -0700

    Update main.py. set player using Player and use updated draw function

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import constants
 
 from constants import SCREEN_HEIGHT, SCREEN_WIDTH
 from logger import log_state
+from player import Player
 
 def main():
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
@@ -13,14 +14,17 @@ def main():
     dt = 0
 
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
     while True: 
         log_state()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill('black')
+        player.draw(screen)
         pygame.display.flip()   
         dt = clock.tick(60)/1000
+    
 
     
 
